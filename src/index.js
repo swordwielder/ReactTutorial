e4618be8bb295ea57b45ce6412b7e1190f268390
Qi <qchen125@gmail.com>
2019-11-06 13:25:07 -0500

    constuctor added

--- src/index.js
+++ src/index.js
@@ -9,11 +9,12 @@ class Square extends React.Component {
       value: null,
     };
   }
-  
+
   render() {
     return (
-      <button className="square" onClick={ () => alert('click')}>
-        {this.props.value}
+      <button className="square"
+        onClick={() => this.setState({value: 'O'})}>
+         {this.state.value}
       </button>
     );
   }
